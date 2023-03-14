91f113b8d194e4eebe2ecab3638a3cf71d22b1c1
hudhamalick <noorulcse@gmail.com>
2023-03-14 09:55:58 +0530

    moviecount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
+export default function App() {
+  const arr = ["mohammad", "rahman", "rizwan"];
+  let persons = [
+    {
+      name: "Mohammad",
+      pic:
+        "https://cdn.kimkim.com/files/a/images/c4232789cea9781694e5962c7d2cb13398f8d84e/big-0e7e6eab6fd05216c7b794bf1706e906.jpg"
+    },
+    {
+      name: "Rahman",
+      pic:
+        "https://s1.it.atcdn.net/wp-content/uploads/2020/12/A-view-of-SantAngelo-in-Ischia-island-in-Italy-800x600.jpg"
+    },
+    {
+      name: "Rizwan",
+      pic:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2021/05/2a7d8pk-e1621272929144-768x431.jpg"
+    }
+  ];
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Movie List and Count </h2>
+
+      <Counter />
+      
+      {/* {arr.map((nm)=> <Welcome name={nm}/>)} */}
+      {persons.map((obj)=> <Welcome name={obj.name} pic={obj.pic}/>  )}
+        
+ 
     </div>
-  );
+  )
 }
 
-export default App;
+function Welcome({name,pic}) {
+  return(
+    <div className="welcome">
+      <h2> Hello {name}</h2>
+      <img src={pic} alt="picture" />
+    </div>
+  )
+}
+
+function Counter() {
+  let [like, setLike] = useState(0);
+  let [dislike, setDislike] =useState(0);
+  return(
+    <div>
+      <button onClick={()=> setLike(like + 1) }>👍 {like}</button>
+      <button onClick={()=>setDislike(dislike + 1)}> 👎 {dislike}</button>
+    </div>
+  )
+}
